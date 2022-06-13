886f10e651bdbb592096fd31218882aae73ee5ed
Leia Harlow <leia.harlow@gmail.com>
2022-06-12 22:00:21 -0700

    completes queries

--- server/models.js
+++ server/models.js
@@ -40,20 +40,75 @@ module.exports = {
     );
   },
 
-  // getOne(productId) {
-  //   return db.connect().then(
-  //     (connection) => {
-  //       const query = `SELECT array_agg(json_build_object('feature', text, 'value', text)) FROM product WHERE id = ${productId} `;
-  //       return connection.query(query)
-  //         .then((res) => {
-  //           connection.release();
-  //           return res.rows[0].array_agg;
-  //         })
-  //         .catch((err) => {
-  //           connection.release();
-  //           throw err;
-  //         });
-  //     },
-  //   );
-  // },
+  getOne(productId) {
+    return db.connect().then(
+      (connection) => {
+        const query = `SELECT json_build_object(
+          'id', p.id,
+          'name', p.name,
+          'slogan', p.slogan,
+          'description', p.description,
+          'category', p.category,
+          'default_price', p.default_price,
+          'features', (SELECT coalesce
+            (array_agg(json_build_object(
+              'feature', f.feature,
+              'value', f.value))
+              '{}')
+              AS features FROM features f WHERE p.id = f.product_id)
+        )
+        FROM product p WHERE p.id = ${productId}`;
+        return connection.query(query)
+          .then((res) => {
+            connection.release();
+            return res.rows[0].json_build_object;
+          })
+          .catch((err) => {
+            connection.release();
+            throw err;
+          });
+      },
+    );
+  },
+
+  getStyles(productId) {
+    return db.connect().then(
+      (connection) => {
+        const query = `SELECT json_build_object(
+          'product_id', ${productId},
+          'results', (SELECT coalesce
+            (array_agg(json_build_object(
+              'style_id', s.id,
+              'name', s.name,
+              'original_price', s.original_price,
+              'sale_price', s.sale_price,
+              'deafult?', s.default?,
+              'photos', (SELECT coalesce
+                (array_agg(json_build_object(
+                  'thumbnail_url', p.thumbnail_url,
+                  'url', p.url
+                )), '{}')
+                AS photos FROM photos p WHERE p.styleId = s.id),
+                'skus', (SELECT coalesce
+                  (json_object_agg(
+                    skus.id, json_build_object(
+                      'quantity': skus.quantity,
+                      'size': skus.size
+                    )), '{}')
+                    AS skusData FROM skus WHERE skus.styleId = s.id)
+            )), '{}')
+            AS results FROM styles s WHERE s.product_id = ${productId})
+        )`;
+        return connection.query(query)
+          .then((res) => {
+            connection.release();
+            return res.rows[0].json_build_object;
+          })
+          .catch((err) => {
+            connection.release();
+            throw err;
+          });
+      },
+    );
+  },
 };
